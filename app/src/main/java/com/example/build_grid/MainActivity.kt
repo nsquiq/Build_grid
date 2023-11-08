1dc69cb5a9306983fe343dc5890a14d476bc8489
Nuray <210103331@stu.sdu.edu.kz>
2023-11-09 01:16:09 +0600

    build

--- app/src/main/java/com/example/build_grid/MainActivity.kt
+++ app/src/main/java/com/example/build_grid/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.build_grid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.build_grid.model.Grid
 import com.example.build_grid.ui.theme.Build_GridTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +42,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GridApp(){
+
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    Build_GridTheme {
-        Greeting("Android")
+fun GridCard(grid: Grid, modifier : Modifier = Modifier){
+    Card(modifier = modifier){
+        Row(){
+            Image(
+                painter = painterResource(grid.imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(68.dp)
+            )
+            Column(){
+                Text(
+                    text = stringResource(id = grid.name),
+                    modifier = Modifier.padding(16.dp)
+                )
+                Row(){
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = grid.counting.toString()
+                    )
+                }
+
+            }
+        }
+
     }
+
 }
